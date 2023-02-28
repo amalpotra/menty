be83476d778663587e20242ee7ae8bbae1059247
Amarjeet Malpotra <97591166+amalpotra@users.noreply.github.com>
2023-02-28 19:02:40 +0530

    Initialize repository

---
+++ src/main/java/com/menty/menty/MentyApplication.java
@@ -0,0 +1,13 @@
+package com.menty.menty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menty/menty/MentyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menty.menty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
